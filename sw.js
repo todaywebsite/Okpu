c9e8d3726bf88fdc4b07bbd75f2451f545848a99
todaywebsite <orbaokpu@gmail.com>
2026-05-25 09:03:21 +0100

    sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,50 @@
-// Okpu Orba Service Worker
-var CACHE = 'okpu-orba-v1';
-var ASSETS = ['/','./index.html'];
+// Okpu Orba Service Worker v2
+var CACHE_NAME = 'okpu-orba-v2';
+var ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
 
 self.addEventListener('install', function(e){
+  console.log('SW installing...');
   e.waitUntil(
-    caches.open(CACHE).then(function(cache){
-      return cache.addAll(ASSETS);
+    caches.open(CACHE_NAME).then(function(cache){
+      return cache.addAll(ASSETS).catch(function(err){
+        console.log('Cache addAll error:', err);
+      });
     })
   );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', function(e){
+  console.log('SW activating...');
   e.waitUntil(
     caches.keys().then(function(keys){
-      return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));
+      return Promise.all(
+        keys.filter(function(k){ return k !== CACHE_NAME; })
+            .map(function(k){ return caches.delete(k); })
+      );
     })
   );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', function(e){
+  if(e.request.method !== 'GET') return;
   e.respondWith(
-    caches.match(e.request).then(function(cached){
-      return cached || fetch(e.request).catch(function(){
-        return caches.match('./index.html');
+    fetch(e.request).then(function(resp){
+      var clone = resp.clone();
+      caches.open(CACHE_NAME).then(function(cache){
+        cache.put(e.request, clone);
+      });
+      return resp;
+    }).catch(function(){
+      return caches.match(e.request).then(function(cached){
+        return cached || caches.match('/index.html');
       });
     })
   );
